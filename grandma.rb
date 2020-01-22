0ac4f4bc35acf99aeaf675cbdc6da255c9d4ffba
Diego Vasquez <botellongo69@outlook.com>
2020-01-22 03:10:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,6 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase
+  if phrase === phrase.upcase and phrase === 'I LOVE YOU GRANDMA!':
+    return 'I LOVE YOU TOO PUMPKIN!'
 end
