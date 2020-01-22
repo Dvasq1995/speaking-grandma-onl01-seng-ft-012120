75427b26b497f7e6df2d5c862668ea96b316e894
Diego Vasquez <botellongo69@outlook.com>
2020-01-22 03:08:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     return 'NO, NOT SINCE 1938!'
   elsif phrase != phrase.upcase
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == 'I LOVE YOU GRANDMA!'
+  elsif phrase == phrase.upcase && phrase == 'I LOVE YOU GRANDMA!'
     return "I LOVE YOU TOO PUMPKIN"
   end
 end
